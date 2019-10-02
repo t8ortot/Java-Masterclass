466bc45a9b0da08b5e8aafb214d55fdc88fba709
James Clark <jimmyjclark@gmail.com>
2019-10-02 15:41:20 -0700

    MegaBytesConverter completed

---
+++ Section 4/MegaBytesConverterExercise/src/MegaBytesConverter.java
@@ -0,0 +1,12 @@
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        int megaBytes = kiloBytes / 1024;
+        int remainder = kiloBytes % 1024;
+
+        if (kiloBytes < 0){
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + remainder + " KB");
+        }
+    }
+}
